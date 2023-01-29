df30d422d2ee88c3a21fe84d0b43f9c52536be4d
Muhammad Habiburrahman <habibcoyy@gmail.com>
2023-01-29 13:43:58 +0700

    Create a new workout

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,48 @@
-const Home = () => {
+import axios from "axios";
+import { useEffect, useState } from "react"
 
+//Components
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
+const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    //Fetch
+    // useEffect(() => {
+    //     const fetchWorkouts = async () => {
+    //         const response = await fetch('/api/workouts')
+    //         const json = await response.json()
+
+    //         if (response.ok) {
+    //             setWorkouts(json);
+    //         }
+    //     }
+
+    //     fetchWorkouts()
+    // }, []);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            try {
+                const res = await axios.get('/api/workouts');
+                const json = res.data;
+                setWorkouts(json);
+            } catch (err) {
+                console.log('Gagal membuat data');
+            }
+        }
+        fetchWorkouts()
+    }, []);
     
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+                ))}
+            </div>
+            <WorkoutForm/>
         </div>
     );
 }
