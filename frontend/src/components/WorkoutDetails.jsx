9338944da5fe93952ac3db24681f497322f49110
Muhammad Habiburrahman <habibcoyy@gmail.com>
2023-01-30 14:17:54 +0700

    Handling Error

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,8 +1,18 @@
+import axios from "axios"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
 const WorkoutDetails = ({ workout }) => {
     const dateFormat = new Date(workout.createdAt).toLocaleDateString('id').toString('MMMM YYYY')
+    const { dispatch } = useWorkoutsContext()
 
-    const handleClick = () => {
-        
+    const handleClick = async () => {
+        try {
+            const res = await axios.delete('/api/workouts/' + workout._id)
+            console.log('Deleted workout')
+            dispatch({type: 'DELETE_WORKOUT', payload: res.data})
+        } catch (err) {
+            
+        }
     }
     
     return (
