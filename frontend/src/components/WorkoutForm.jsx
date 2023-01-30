6030057b02f2355f19149c1693c6b76d8cffdb8e
Muhammad Habiburrahman <habibcoyy@gmail.com>
2023-01-30 14:33:55 +0700

    Handling Error Frontend

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -8,6 +8,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState(null)
+    const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -36,11 +37,16 @@ const WorkoutForm = () => {
         
         try {
             const res = await axios.post('/api/workouts', workout)
+            setTitle('')
+            setLoad('')
+            setReps('')
             setError(null)
+            setEmptyFields([])
             console.log('new workout added')
             dispatch({type: 'CREATE_WORKOUT', payload: res.data})
         } catch (err) {
             setError(err.response.data.error)
+            setEmptyFields(err.response.data.emptyFields)
         }
     }
 
@@ -52,18 +58,21 @@ const WorkoutForm = () => {
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? 'error' : ''}
             />
             <label>Load (in kg):</label>
             <input 
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
             <label>Reps:</label>
             <input 
                 type="text"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
             <button>Add Workout</button>
             {error && <div className="error">{error}</div>}
